045086a82322ecbd2bee36854c69d9205c7b0080
Jeff Roberson <jmrware@gmail.com>
2010-09-06 09:23:36 -0600

    20100906_0900 Added documentation and tidied up a bit.

---
+++ README.txt
@@ -0,0 +1,44 @@
+/* File:        README for linkify.js and linkify.php
+ * Version:     20100906_0900
+ * Copyright:   (c) 2010 Jeff Roberson - http://jmrware.com
+ * MIT License: see: http://www.opensource.org/licenses/mit-license.php
+ */
+
+Summary:  This project consists of 2 script files: linkify.js and linkify.php.
+Each of these scripts contain a function called: "linkify()" which converts
+all URLs (HTTP, HTTPS, FTP and FTPS)) in a passed string into equivalent HTML
+link tags. i.e. Given the input string: 'Go http://example.com here!', the
+function returns: 'Go <a href="http://test.com">http://test.com"<a> here!'.
+The linkify function does not touch URLs that are already part of a HTML or
+BBCode link. e.g. 
+
+File Descriptions:
+-----------
+ linkify.js
+-----------
+Javascript file containing the linkify() function. It also has a few support
+functions which allow the user to clink on elements of the linkify.html page
+to linkify their contents interactively. Once linkified the script checks to
+see if the resulting URL links contain any unbalanced parentheses or square
+brackets and displays the ones which are unbalanced in red.
+
+------------
+ linkify.php
+------------
+PHP script file containing the linkify() function. It also has code which
+reads the linkify.html file, linkifies all the contents of its <body>
+element, then displays the converted web page.
+
+------------
+ linkify.rbl
+------------
+This is a RegexBuddy library file containing the regular expression
+used by the scripts. This tool was used extensively during development.
+Highly recommended. See: http://www.regexbuddy.com/.
+
+-------------
+ linkify.html
+-------------
+This web page contains many un-linked URLs which are used to test and
+demonstrate the linkify.js and linkify.php scripts. If also provides
+a detailed commented listing of the regular expression used.
--- linkify.php
+++ linkify.php
@@ -1,50 +1,50 @@
 <?php
-/* File:		linkify.php
- * Version:		20100906_0300
- * Copyright:	(c) 2010 Jeff Roberson - http://jmrware.com
- * MIT License:	http://www.opensource.org/licenses/mit-license.php
+/* File:        linkify.php
+ * Version:     20100906_0900
+ * Copyright:   (c) 2010 Jeff Roberson - http://jmrware.com
+ * MIT License: http://www.opensource.org/licenses/mit-license.php
  *
  * Summary: This script linkifys http URLs on a page.
  *
- * Usage:	See example page: linkify.html
+ * Usage:   See example page: linkify.html
  */
+function linkify($text) {
+    $url_pattern = '/
+    # url_pattern: Match http(s)\/ftp(s) URL that is not already linkified.
+      (\()                     # $1: URL delimited by (parentheses).
+      ((?:ht|f)tps?:\/\/[A-Za-z0-9\-._~:\/?#[\]@!$&\'()*+,;=%]+)  # $2: URL.
+      (\))                     # $3: ")" end delimiter.
+    | (\[)                     # $4: URL delimited by [square brackets].
+      ((?:ht|f)tps?:\/\/[A-Za-z0-9\-._~:\/?#[\]@!$&\'()*+,;=%]+)  # $5: URL.
+      (\])                     # $6: "]" end delimiter.
+    | (\{)                     # $7: URL delimited by {curly brackets}.
+      ((?:ht|f)tps?:\/\/[A-Za-z0-9\-._~:\/?#[\]@!$&\'()*+,;=%]+)  # $8: URL.
+      (\})                     # $9: "}" end delimiter.
+    | (<|&(?:lt|\#60|\#x3c);)  # $10: URL delimited by <angle brackets>.
+      ((?:ht|f)tps?:\/\/[A-Za-z0-9\-._~:\/?#[\]@!$&\'()*+,;=%]+)  # $11: URL.
+      (>|&(?:gt|\#62|\#x3e);)  # $12: ">" end delimiter. (HTML entities too).
+    | (                        # $13: Prefix proving URLs not already linked.
+        (?: ^                  # Can be a beginning of line\/ string or
+        | [^=\s\'"\]]          # a non-equals, non-quote, followed by
+        ) \s*[\'"]?            # optional whitespace and optional quote;
+      | [^=\s]\s+              # or... a non-equals sign followed by whitespace.
+      )                        # End $13. Non-prelinkified-proof prefix.
+      ( \b                     # $14: Match other URLs.
+        (?:ht|f)tps?:\/\/      # Required literal http URL prefix
+        [A-Za-z0-9\-._~:\/?#[\]@!$\'()*+,;=%]* # Unroll-the-loop. (normal*)
+        (?: (?!(?:&(?:gt|apos|quot|\#0*(?:34|39|62)|\#x0*(?:22|27|3e));))
+          &                    # Match "&" if not an ending HTML entity (special).
+          [A-Za-z0-9\-._~:\/?#[\]@!$\'()*+,;=%]* # more normal*
+        )*                     # Unroll-the-loop (special normal*)*.
+        [A-Za-z0-9\-_~\/#[\]@$()*+;=%] # Last char can\'t be [.:?!&\',]
+      )                        # End $14. Other URLs.
+    /imx';
+    $url_replace = '$1$4$7$10$13<a href="$2$5$8$11$14">$2$5$8$11$14</a>$3$6$9$12';
+    return preg_replace($url_pattern, $url_replace, $text);
+}
+// Linkify and display the linkify.html file.
 $text = file_get_contents('linkify.html');
 preg_match('/^(.*?<body[^>]*>)(.*)$/si', $text, $matches);
 $text = $matches[2];
-
-function linkify($text) {
-	$url_pattern = '/
-# url_pattern: Match http(s)\/ftp(s) URL that is not already linkified.
-  (\()                     # $1: URL delimited by (parentheses).
-  ((?:ht|f)tps?:\/\/[A-Za-z0-9\-._~:\/?#[\]@!$&\'()*+,;=%]+)  # $2: URL.
-  (\))                     # $3: ")" end delimiter.
-| (\[)                     # $4: URL delimited by [square brackets].
-  ((?:ht|f)tps?:\/\/[A-Za-z0-9\-._~:\/?#[\]@!$&\'()*+,;=%]+)  # $5: URL.
-  (\])                     # $6: "]" end delimiter.
-| (\{)                     # $7: URL delimited by {curly brackets}.
-  ((?:ht|f)tps?:\/\/[A-Za-z0-9\-._~:\/?#[\]@!$&\'()*+,;=%]+)  # $8: URL.
-  (\})                     # $9: "}" end delimiter.
-| (<|&(?:lt|\#60|\#x3c);)  # $10: URL delimited by <angle brackets>.
-  ((?:ht|f)tps?:\/\/[A-Za-z0-9\-._~:\/?#[\]@!$&\'()*+,;=%]+)  # $11: URL.
-  (>|&(?:gt|\#62|\#x3e);)  # $12: ">" end delimiter. (HTML entities too).
-| (                        # $13: Prefix proving URLs not already linked.
-    (?: ^                  # Can be a beginning of line\/ string or
-    | [^=\s\'"\]]          # a non-equals, non-quote, followed by
-    ) \s*[\'"]?            # optional whitespace and optional quote;
-  | [^=\s]\s+              # or... a non-equals sign followed by whitespace.
-  )                        # End $13. Non-prelinkified-proof prefix.
-  ( \b                     # $14: Match other URLs.
-    (?:ht|f)tps?:\/\/      # Required literal http URL prefix
-    [A-Za-z0-9\-._~:\/?#[\]@!$\'()*+,;=%]* # Unroll-the-loop. (normal*)
-    (?: (?!(?:&(?:gt|apos|quot|\#0*(?:34|39|62)|\#x0*(?:22|27|3e));))
-      &                    # Match "&" if not an ending HTML entity (special).
-      [A-Za-z0-9\-._~:\/?#[\]@!$\'()*+,;=%]* # more normal*
-    )*                     # Unroll-the-loop (special normal*)*.
-    [A-Za-z0-9\-_~\/#[\]@$()*+;=%] # Last char can\'t be [.:?!&\',]
-  )                        # End $14. Other URLs.
-/imx';
-$url_replace = '$1$4$7$10$13<a href="$2$5$8$11$14">$2$5$8$11$14</a>$3$6$9$12';
-	return preg_replace($url_pattern, $url_replace, $text);
-}
 echo($matches[1]. linkify($text));
 ?>
