65a73eb62dbb258b84bd9660c1390959f2831ac7
Jeff Roberson <jmrware@gmail.com>
2010-10-09 08:26:02 -0600

    20101009_0800 Fixed optional <script> links, added MIT-LICENSE.

--- linkify.js
+++ linkify.js
@@ -1,6 +1,6 @@
 /* <![CDATA[ */
 /* File:        linkify.js
- * Version:     20100913_0900
+ * Version:     20101009_0800
  * Copyright:   (c) 2010 Jeff Roberson - http://jmrware.com
  * MIT License: http://www.opensource.org/licenses/mit-license.php
  *
